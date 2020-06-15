c5e763d61eac838b6cd8e7caefa9873e8b2f4eed
Brayan Llanos <brayan0428@gmail.com>
2020-06-14 22:03:19 -0500

    Se crearon los componentes de PhotoCard y ListOfPhotoCards, tambien se añadio el Logo y una animación

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyles } from './globalStyles'
+import { GlobalStyles } from './styles/globalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 const App = () => (
   <>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
 
