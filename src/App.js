d18d825a8e47e7027980bf894d4c09fc41da1981
Brayan Llanos <brayan0428@gmail.com>
2020-06-14 21:31:59 -0500

    Se crearon los componentes de Category y ListOfCategories con sus respectivos estilos, y ademas se añadieron los estilos globales

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyles } from './globalStyles'
+
+const App = () => (
+  <>
+    <GlobalStyles />
+    <ListOfCategories />
+  </>
+)
+
+export default App
